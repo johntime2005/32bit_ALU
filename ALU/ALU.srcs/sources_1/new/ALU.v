bb211f5b26b60cf42bd26e99ae43c5f59f944d8c
johntime <johntimeson@outlook.com>
2024-12-02 18:41:32 +0800

    设置日程安排

--- ALU/ALU.srcs/sources_1/new/ALU.v
+++ ALU/ALU.srcs/sources_1/new/ALU.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2024/12/01 18:12:19
+// Create Date: 2024/12/01 19:15:31
 // Design Name: 
 // Module Name: ALU
 // Project Name: 
@@ -20,7 +20,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU(
-
-    );
+module ALU (
+    OP,
+    A,
+    B,
+    RESULT
+);
+    parameter SIZE = 31;
+    input [2:0] OP;
+    input [SIZE:0] A;
+    input [SIZE:0] B;
+    output [SIZE:0] RESULT;
+    
 endmodule
